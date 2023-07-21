12521779186210991663c49d8f5c78a049df6734
DOBLAMOSBACK-END <practicante.ti@doblamos.net>
2023-07-21 16:59:29 -0500

    Agregar frontend de Vue.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import VueSweetalert2 from 'vue-sweetalert2';
+window.$ = window.jQuery = require('jquery');
+import 'datatables.net-dt/css/jquery.dataTables.css';
+import 'datatables.net-responsive-dt/css/responsive.dataTables.css';
+import 'select2/dist/css/select2.min.css';
+import 'select2-bootstrap-theme/dist/select2-bootstrap.min.css';
 
-createApp(App).use(store).use(router).mount('#app')
+// Configurar Laravel Echo
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: 'f58064020a5f1a695b6d',
+  cluster: 'us2',
+  encrypted: true,
+});
+
+const app = createApp(App);
+
+// Uso del plugin
+app.use(VueSweetalert2);
+
+app.use(store).use(router).mount('#app');
+
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import Chart from 'chart.js';
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,105 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import MesaCreate from '../views/Mesas/MesaCreate.vue';
+import PedidoLiberados from '../views/Pedidos/PedidoLiberados.vue';
+import RegistrarPedido from '../views/Pedidos/RegistrarPedido.vue';
+import PedidosCocina from '../views/Pedidos/PedidosCocina.vue';
+import loginautenticate from '../views/Login/loginautenticate.vue';
+import categoriaList from '../views/Categorias/categoriaList.vue';
+import ProductosList from '../views/Productos/ProductosList.vue';
+import pagos from '../views/PagosGenerados/pagos.vue';
+import pagosVue from '../views/PagosGenerados/pagos.vue';
 const routes = [
+  {
+    path: '/Login',
+    name: 'loginautenticate',
+    component: loginautenticate,
+  },
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiresAuth: true, // Establece la propiedad requiresAuth como true para requerir autenticación en la ruta
+    },
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: '/Mesas',
+    name: 'CrearMesa',
+    component: MesaCreate,
+    meta: {
+      requiresAuth: true, // Requiere autenticación en esta ruta
+    },
+  },
+  {
+    path: '/Categorias',
+    name: 'ListarCategorias',
+    component: categoriaList,
+    meta: {
+      requiresAuth: true, // Establece la propiedad requiresAuth como true para requerir autenticación en la ruta
+    },
+  },
+
+  {
+    path: '/pagos-generados',
+    name: 'pagos',
+    component: pagos,
+    meta: {
+      requiresAuth: true, // Establece la propiedad requiresAuth como true para requerir autenticación en la ruta
+    },
+  },
+  {
+    path: '/Productos',
+    name: 'ListarProductos',
+    component: ProductosList,
+    meta: {
+      requiresAuth: true,  // Establece la propiedad requiresAuth como true para requerir autenticación en la ruta
+    },
+  },
+
+  {
+    path: '/pedidos-liberados',
+    name: 'ListaPedido',
+    component: PedidoLiberados,
+    meta: {
+      requiresAuth: true, // Requiere autenticación en esta ruta
+    },
+  },
+
+  {
+    path: '/pedidos-cocina',
+    name: 'pedidoscocina',
+    component: PedidosCocina,
+    meta: {
+      requiresAuth: true, // Requiere autenticación en esta ruta
+    },
+  },
+  {
+    path: '/Registrar-Pedidos',
+    name: 'Registrar-pedidos',
+    component: RegistrarPedido,
+    meta: {
+      requiresAuth: true, // Requiere autenticación en esta ruta
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = !!localStorage.getItem('token'); // Verifica si hay un token de acceso en localStorage
+
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    next('/Login'); // Redirige al inicio de sesión si la ruta requiere autenticación y el usuario no está autenticado
+  } else if (to.name === 'loginautenticate' && isLoggedIn) {
+    next('/'); // Si el usuario ya ha iniciado sesión y está intentando acceder a la página de inicio de sesión, redirige a la vista "home"
+  } else {
+    next(); // Continúa navegando a la siguiente ruta
+  }
+});
+
+export default router;
